a1a8e06e4d1896223b33396e63c228786dfb8ec6
atrevisanut <atrevisanut@gmail.com>
2017-04-04 16:11:31 -0600

    answers for js basics pt 3

--- test.js
+++ test.js
@@ -12,13 +12,23 @@ var candies = {
   mugrootbeer: 'not candy... but the best'
 };
 
+for(var key in candies){
+  if(key.charAt(0) === 'm'){
+    console.log(this.key + "  hit")
+    candies[key] =  "no m&m's for you";
+  }
+}
+
 /////////////
 // Num. 6  //////////////////////////////////////////////////////////
 // Create a fuction called getName that prompts the user for their //
 // name, store that name in a var called name. Return "hello"      //
 /////////////////////////////////////////////////////////////////////
 
-
+function getName(){
+  var name = window.prompt("what's your name?")
+  return "hello"
+}
 
 /////////////
 // Num. 7  //////////////////////////////////////////////////////////
@@ -28,3 +38,16 @@ var candies = {
 // number of vowels in that return value. Have I said return value //
 // enough?                                                         //
 /////////////////////////////////////////////////////////////////////
+
+function vowelCounter(func){
+  var str = func.split('');
+  var counter = 0;
+  for(var i = 0; i < str.length; i++){
+    if(str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u"){
+      counter += 1;
+    }
+  }
+  return counter;
+}
+
+vowelCounter(getName())
