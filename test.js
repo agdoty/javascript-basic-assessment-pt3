ee0c86e4fb7c9340a0ac98046524dfb91d81488d
atrevisanut <atrevisanut@gmail.com>
2017-04-04 16:16:55 -0600

    took out 1 q

--- test.js
+++ test.js
@@ -1,25 +1,4 @@
 /////////////
-// Num. 5  //////////////////////////////////////////////////////////
-// Create a function called objectLooper that will go through the  //
-// given object below and change all keys that start with 'm' to   //
-// have a value of "no m&m's for you"                              //
-/////////////////////////////////////////////////////////////////////
-var candies = {
-  mAndm: 'delicious',
-  kitkat: 'to die for',
-  skittles: 'meh',
-  marsbars: 'never tried',
-  mugrootbeer: 'not candy... but the best'
-};
-
-for(var key in candies){
-  if(key.charAt(0) === 'm'){
-    console.log(this.key + "  hit")
-    candies[key] =  "no m&m's for you";
-  }
-}
-
-/////////////
 // Num. 6  //////////////////////////////////////////////////////////
 // Create a fuction called getName that prompts the user for their //
 // name, store that name in a var called name. Return "hello"      //
